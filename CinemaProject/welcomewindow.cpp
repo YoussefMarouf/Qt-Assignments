c9973dffe4cb095b68fed1dd1fc443dcdedbc720
Anas Sabry <anassabry1@aucegypt.edu>
2024-03-05 23:20:28 +0200

    Added echo mode and adjusted combobox

--- CinemaProject/welcomewindow.cpp
+++ CinemaProject/welcomewindow.cpp
@@ -8,7 +8,7 @@ WelcomeWindow::WelcomeWindow(QWidget *parent,QString name ,int age)
     , ui(new Ui::WelcomeWindow)
 {
     ui->setupUi(this);
-    QPixmap pix ("C:\\Users\\youse\\Desktop\\Spring 24\\Cs 2\\Codes\\Qt-Assignments\\CinemaProject\\Imgs\\Cinema.jpg");
+    QPixmap pix ("D:\Qt-Assignments\Qt-Assignments\CinemaProject\Imgs\Cinema.jpg");
     QString str = QString :: number ( age );
     ui->label_welcome->setText("Hello, "  + name + " " + str );
     int w=ui->label_image->width();

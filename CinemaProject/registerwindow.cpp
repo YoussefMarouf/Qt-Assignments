1f94aa9a92c6bd388e54661883faf3cdb480f1a1
Anas Sabry <anassabry1@aucegypt.edu>
2024-03-05 22:49:33 +0200

    Finished extracting data validation required

--- CinemaProject/registerwindow.cpp
+++ CinemaProject/registerwindow.cpp
@@ -16,3 +16,26 @@ RegisterWindow::~RegisterWindow()
 {
     delete ui;
 }
+
+void RegisterWindow::on_pushButton_clicked()
+{
+    QString un=ui->lineEdit_Username->text();
+    QString pass=ui->lineEdit_password->text();
+    QString repass=ui->lineEdit_password->text();
+    QString day=ui->comboBox_Day->currentText();
+    QString month=ui->comboBox_Month->currentText();
+    QString year=ui->comboBox_Year->currentText();
+    bool male=ui->radioButton_Male->isChecked();
+    bool female=ui->radioButton_Female->isChecked();
+    bool admin=ui->radioButton_Admin->isChecked();
+    bool user=ui->radioButton_User->isChecked();
+    bool action=ui->checkBox_Action->isChecked();
+    bool comedy=ui->checkBox_Comedy->isChecked();
+    bool drama=ui->checkBox_Drama->isChecked();
+    bool romance=ui->checkBox_Romance->isChecked();
+    bool horror=ui->checkBox_Horror->isChecked();
+    bool other=ui->checkBox_Other->isChecked();
+
+
+}
+
